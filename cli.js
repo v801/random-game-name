7fd4a2699a86c98436dc3d44ad97ef2f80411de4
v801 <32247825+v801@users.noreply.github.com>
2022-07-16 04:49:52 -0600

    add basic help command

--- cli.js
+++ cli.js
@@ -14,4 +14,21 @@ program
     console.log(randomGameName());
   });
 
+program.command('help')
+  .description('Displays help info about random-game-name.')
+  .action(() => {
+    console.log(
+`random-game-name
+Get a random video game name
+
+Usage
+$ random-game-name
+
+Examples
+$ random-game-name
+Inept Caveman Overload
+`
+    )
+  })
+
 program.parse();
--- readme.md
+++ readme.md
@@ -39,7 +39,7 @@ $ npm install -g v801/random-game-name
 ```
 
 ```
-$ random-game-name
+$ random-game-name help
 
   Get a random video game name
 
